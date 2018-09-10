d17052b99996b656f98b878a3b8a63218fea70a1
NathOrmond <nath.m.ormond@gmail.com>
2018-09-10 15:58:44 +0100

    unit test

--- test/analysis/datatypes/DataPricesTest.java
+++ test/analysis/datatypes/DataPricesTest.java
@@ -13,6 +13,7 @@ import org.junit.Assert;
  * @author Nathan Ormond
  *
  * Run as jUnit 4 test (jUnit 5 will cause to crash/fail)
+ * Test runs on stimulated data within file.
  *
  */
 public class DataPricesTest {
@@ -33,46 +34,12 @@ public class DataPricesTest {
 		dp = new DataPrices();
 	}
 
-	@Test
-	public void testIterationMethod() {
-		String testName = "ITERATION_METHOD_TEST :";
-		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
-
-		System.out.println(consoleOutput(testName, "START"));
-
-		//ToDo 
-		//Contains ListPrice && CostCount (stim data +ve and -ve) 
-		//JSONArrayLen < ListLen {addNew} else {dontAddNew}
-		
-		// Not Yet Implemented
-		Assert.assertTrue(false);
-
-		String status = null;
-//		status = expectedData.equals(someCondition) ? "PASS" : "FAIL";
-		System.out.println(consoleOutput(testName, status));
-	}
-
-	@Test
-	public void testEvaluateListForRanking() {
-		String testName = "EVALUATE_LIST_FOR_RANKING_TEST :";
-		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
-
-		System.out.println(consoleOutput(testName, "START"));
-
-		// Not Yet Implemented
-		Assert.assertTrue(false);
-
-		String status = null;
-//		status = expectedData.equals(someCondition) ? "PASS" : "FAIL";
-		System.out.println(consoleOutput(testName, status));
-	}
-
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testCreateDesiredObject() {
 		String testName = "CREATE_DESIRED_OBJECT_TEST :";
-		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
-		System.out.println(consoleOutput(testName, "START"));
+		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName,0));
+		System.out.println(consoleOutput(testName, "START",4));
 		JSONObject expectedObj = new JSONObject();
 		expectedObj.put("costPerPage", 0.009032258064516128);
 		expectedObj.put("title", "COMPUTER PROGRAMMING IN C");
@@ -84,36 +51,76 @@ public class DataPricesTest {
 				}
 		}
 		JSONObject returnedObj = dp.createDesiredObject(item);
-		System.out.println("\nExpected");
-		System.out.println(expectedObj.toJSONString());
-		System.out.println("\nActual");
-		System.out.println(returnedObj.toJSONString());
+		System.out.println(consoleOutput("EXPECTED" , expectedObj.toJSONString(), 8));
+		System.out.println(consoleOutput("ACTUAL  ",returnedObj.toJSONString(),8));
 		Assert.assertEquals(expectedObj.get("costPerPage"), returnedObj.get("costPerPage"));
 		Assert.assertEquals(expectedObj.get("title"), returnedObj.get("title"));
 		String status = null;
 		status = ((double) expectedObj.get("costPerPage") == (double) returnedObj.get("costPerPage")) && ((expectedObj.get("title").equals(returnedObj.get("title")))) ? "PASS" : "FAIL";
-		System.out.println(consoleOutput(testName, status));
+		System.out.println(consoleOutput(testName, status,4));
+		System.out.println(consoleOutput("END DATA PRICES TEST", testName,0));
 	}
 
 	@Test
 	public void testGetRankedArray() throws ParseException {
 		String testName = "GET_RANKED_ARRAY_TEST :";
-		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
-		System.out.println(consoleOutput("testname", "START"));
+		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName,0));
+		System.out.println(consoleOutput(testName , "START",4));
 		JSONArray returnedArray = dp.getRankedArray(5);
 		String returnedData = returnedArray.toJSONString();
-		System.out.println("\nExpected : ");
-		System.out.println(expectedData);
-		System.out.println("\nActual : ");
-		System.out.println(returnedData);
+		System.out.println(consoleOutput("EXPECTED" , expectedData, 8));
+		System.out.println(consoleOutput("ACTUAL  ",returnedData,8));
 		Assert.assertEquals(expectedData, returnedData);
 		String status;
 		status = expectedData.equals(returnedData) ? "PASS" : "FAIL";
-		System.out.println(consoleOutput(testName, status));
+		System.out.println(consoleOutput(testName, status,4));
+		System.out.println(consoleOutput("END DATA PRICES TEST", testName,0));
 	}
 
-	private String consoleOutput(String content, String suffix) {
-		return "\n#### " + content + " #### [STATUS: " + suffix + "]";
+	private String consoleOutput(String content, String suffix, int indentation) {
+		String indnt = "";
+		for(int i = 0; i < indentation; i++) { 
+			indnt = indnt + " ";
+		}
+		
+		return "\n" + indnt +"#### " + content + " #### [STATUS: " + suffix + "]";
 	}
+	
+	
+		/** TESTS TO BE IMPLEMENTED **/
+	
+//	@Test
+//	public void testIterationMethod() {
+//		String testName = "ITERATION_METHOD_TEST :";
+//		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
+//
+//		System.out.println(consoleOutput(testName, "START"));
+//
+//		//ToDo 
+//		//Contains ListPrice && CostCount (stim data +ve and -ve) 
+//		//JSONArrayLen < ListLen {addNew} else {dontAddNew}
+//		
+//		// Not Yet Implemented
+//		Assert.assertTrue(false);
+//
+//		String status = null;
+////		status = expectedData.equals(someCondition) ? "PASS" : "FAIL";
+//		System.out.println(consoleOutput(testName, status));
+//	}
+//
+//	@Test
+//	public void testEvaluateListForRanking() {
+//		String testName = "EVALUATE_LIST_FOR_RANKING_TEST :";
+//		System.out.println(consoleOutput("NEW DATA PRICES TEST", testName));
+//
+//		System.out.println(consoleOutput(testName, "START"));
+//
+//		// Not Yet Implemented
+//		Assert.assertTrue(false);
+//
+//		String status = null;
+////		status = expectedData.equals(someCondition) ? "PASS" : "FAIL";
+//		System.out.println(consoleOutput(testName, status));
+//	}
 
 }
